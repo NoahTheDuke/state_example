96a7346ac4d82c37c63149b49f9cfc1acf07cb57
Noah Bogart <noah.bogart@hey.com>
2023-03-30 14:23:31 -0400

    Add .gitignore

--- state_example.py
+++ state_example.py
@@ -1,15 +1,5 @@
-from state_example import stateful, pure
-
-
-def main():
-    print("running stateful example twice")
-    stateful.main()
-    stateful.main()
-
-    print("running pure example twice")
-    pure.main()
-    pure.main()
+from state_example import __main__ as sm
 
 
 if __name__ == "__main__":
-    main()
+    sm.main()
